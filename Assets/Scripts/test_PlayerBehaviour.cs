dd964be65ad535250082999f4c67366cf86fbea8
vegs <vegard.storm@gmail.com>
2018-07-09 23:23:25 +0200

    photon

---
+++ Assets/Scripts/test_PlayerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_PlayerBehaviour : MonoBehaviour {
+
+    public float speed = 1f;
+
+    void Update()
+    {
+        InputMovement();
+    }
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            this.transform.position += Vector3.forward * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.S))
+            this.transform.position -= Vector3.forward * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            this.transform.position -= Vector3.right * speed * Time.deltaTime;
+        
+    }
+}
